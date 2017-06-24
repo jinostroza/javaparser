b84eeb62bb16b3f5274137fd7e39d0e480d39748
Jorge Inostroza <jorge.inostroza.u@gmail.com>
2017-06-23 22:54:03 -0400

    cambios en la estructura del proyecto, se agregan libs java poi para el correcto funcionamiento

--- src/cl/deloitte/wsib/ExcelReader.java
+++ src/cl/deloitte/wsib/ExcelReader.java
@@ -2,35 +2,55 @@ package cl.deloitte.wsib;
 
 import java.io.*;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import jxl.read.biff.BiffException;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
 
 /**
  * Created by jinostrozau on 2017-06-23.
  */
 public class ExcelReader {
 
-    private String path = "/JIU/AcesViewer.xlsx";
+    private String path = "/JIU/Test.xls";
     //private String path = "C://JIU/WSIB/Reportes/AcesViewer Unnumbered DocsAnalysis.xlsx";
 
     public static void main(String[] args){
 
-        new ExcelReader().leeExcel();
-
-    }
-
-    public void leeExcel(){
-
-        try{
-            Workbook wb = WorkbookFactory.create(new File(path));
-            System.out.println(wb.getSheetAt(0).getRow(0).getCell(0));
-        }catch(FileNotFoundException e){
+        try {
+            new ExcelReader().writeExcel();
+        } catch (WriteException e) {
             e.printStackTrace();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }catch(InvalidFormatException e){
+        } catch (BiffException e) {
             e.printStackTrace();
         }
+
     }
+
+
+    public void writeExcel() throws WriteException, IOException, BiffException {
+        jxl.Workbook wb = jxl.Workbook.getWorkbook(new File(path));
+        WritableWorkbook copy = jxl.Workbook.createWorkbook(new File(path), wb);
+
+        WritableSheet copySheet = copy.getSheet(0);
+
+
+        int lastRow = copySheet.getRows();
+        System.out.println("Last row: "+copySheet.getRows());
+
+        Label anotherWritableCell =  new Label(0,lastRow,"125");
+
+
+        copySheet.addCell(anotherWritableCell);
+
+        //aCopySheet.addCell(anotherWritableCel2);
+
+        copy.write();
+        copy.close();
+
+    }
+
 }
